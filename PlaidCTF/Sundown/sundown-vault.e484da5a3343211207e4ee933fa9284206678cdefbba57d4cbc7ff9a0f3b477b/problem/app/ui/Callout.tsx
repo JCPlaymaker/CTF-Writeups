3b4a97c4e185fb50fac8c1479a789b6d12257861
JayCee <jaycee@JayCee.>
2025-05-01 14:06:52 -0400

    removed unsolved chall

--- PlaidCTF/Sundown/sundown-vault.e484da5a3343211207e4ee933fa9284206678cdefbba57d4cbc7ff9a0f3b477b/problem/app/ui/Callout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import styles from "./Callout.module.scss";
-
-interface Props {
-	children: React.ReactNode;
-}
-
-export const Callout = (props: Props) => <div className={styles.callout}>{props.children}</div>;
